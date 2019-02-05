70ebbe492f748462e7d027f33eefc4c1299cdc40
Trần Thị Ngọc Trâm <eve@Eve.local>
2019-02-06 00:16:15 +0700

    Remove github calendar

--- src/Background/Background.js
+++ src/Background/Background.js
@@ -2,12 +2,10 @@ import React from 'react'
 import './Background.scss'
 import {Card, List, Avatar} from 'antd'
 import PropTypes from 'prop-types'
-import ReactGithubCalendar from '@axetroy/react-github-calendar'
 
 const Background = ({data}) => {
   return (
     <div className='background'>
-      {window.innerWidth > 768 && <ReactGithubCalendar name='TramTran2496'/>}
       {data.map((item, index) =>
         <Card title={item.title} key={index} bordered={false}>
           <List itemLayout='horizontal'
