e51368520c78c6060f4cefe8652b9ff54ba5f55d
Nhuong Hoang <nhuongmh@gmail.com>
2023-03-03 02:15:12 +0700

    update WIP march 23

--- src/App.js
+++ src/App.js
@@ -3,73 +3,69 @@ import "./App.css";
 import Header from "./Header/Header";
 import BasicInfo from "./BasicInfo/BasicInfo";
 import Background from "./Background/Background";
-import unimelb from "./img/unimelb.png";
 import bku from "./img/bku.png";
-import ved from "./img/vietnam-esports-logo.jpg";
-import vng from "./img/vng.png";
-import smartdatics from "./img/smartdatics.png";
+import ampere from "./img/ampere.png";
+import ampere_char from "./img/ampere-char.png"
+import arrive from "./img/arrive.webp";
 import {
   GithubFilled,
   LinkedinFilled,
-  FacebookFilled,
-  InstagramFilled,
+  TwitterSquareFilled,
   MailOutlined,
-  PhoneOutlined
+  PhoneOutlined,
+  DeploymentUnitOutlined
 } from "@ant-design/icons";
 
 const App = () => {
   const headerData = {
-    name: { first: "Tram", last: "Tran Thi Ngoc" },
-    career: "Student"
+    name: { first: "Nhuong", last: "Hoang Minh" },
+    career: "Software Engineer"
   };
   const basicData = {
     social: [
-      { icon: <GithubFilled />, link: "https://github.com/TramTran2496" },
+      { icon: <GithubFilled />, link: "https://github.com/nhuongmh" },
       {
         icon: <LinkedinFilled />,
-        link: "https://www.linkedin.com/in/tr%C3%A2m-tr%E1%BA%A7n-27a521112/"
+        link: "https://www.linkedin.com/in/nhuongmh/",
       },
       {
-        icon: <FacebookFilled />,
-        link: "https://www.facebook.com/tram.tranthingoc.2496"
+        icon: <TwitterSquareFilled />,
+        link: "https://twitter.com/nhuongmh",
       },
-      {
-        icon: <InstagramFilled />,
-        link: "https://www.instagram.com/hilary.eve"
-      }
     ],
     contact: [
-      { icon: <MailOutlined />, contact: "ttntram2496@gmail.com", type: "mailto:" },
-      { icon: <PhoneOutlined />, contact: "+84704113755", type: "tel:" }
+      {
+        icon: <MailOutlined />,
+        contact: "nhuongmh@gmail.com",
+        type: "mailto:",
+      },
+      { icon: <PhoneOutlined />, contact: "+84981683925", type: "tel:" },
+      {
+        icon: <DeploymentUnitOutlined />,
+        contact: "minhnhuong.com",
+        type: "https://"
+      }
     ],
     information: [
-      { title: "Birthday", value: "02 Apr 1996" },
-      { title: "Gender", value: "female" },
-      { title: "Experiences", value: "3 years" }
+      { title: "Birthday", value: "06 Jun 1996" },
+      { title: "Location", value: "D7, HCMC" },
+      { title: "Experiences", value: "4 years" },
     ],
     objective:
       "To obtain a position that will enable me to utilize my problem solving skills and attention to detail to further develop my abilities in the field of computer science.",
     language: [
-      { title: "Vietnamese", value: "native" },
-      { title: "English", value: "fluent" }
-    ]
+      { title: "English", value: "good" },
+    ],
   };
   const background = [
     {
       title: "EDUCATION",
       list: [
         {
-          title: "The University of Melbourne",
-          subtitle: "Master of Computer Science",
-          daterange: "March 2021 - present",
-          description: [],
-          logo: unimelb
-        },
-        {
           title: "Ho Chi Minh University of Technology",
-          subtitle: "Computer Science",
+          subtitle: "Electrical & Electronics Engineering",
           daterange: "Sep 2014 - Jun 2018",
-          description: ["Honor Program", "GPA: 8.37/10"],
+          description: ["GPA: 7.5/10"],
           logo: bku
         }
       ]
@@ -78,36 +74,25 @@ const App = () => {
       title: "WORK EXPERIENCE",
       list: [
         {
-          title: "Vietnam Esports",
-          subtitle: "Front-end Developer",
-          daterange: "Nov 2018 - February 2021",
+          title: "Ampere Computing VN",
+          subtitle: "Platform Validation Engineer",
+          daterange: "Nov 2018 - Present",
           description: [
             "Here, I build support tools for Shopee and some website for their partners. As the position is front-end developer, I design UI, UX to develop the features that users require.",
             "I have contributed significant projects to Shopee business in Vietnam.",
             "Use: Javascript, Html, Sass, React, NodeJS, Webpack"
           ],
-          logo: ved
+          logo: ampere_char
         },
         {
-          title: "VNG Corporation",
-          subtitle: "Front-end Developer",
-          daterange: "Aug 2017 - Oct 2018",
+          title: "Arrive Technologies",
+          subtitle: "Logic Design Engineer",
+          daterange: "Jun 2018 - Nov 2018",
           description: [
-            "At Zalo Team, I joined in project Zalo Official Account. I work mostly in front-end part and a little in back-end. I learned about server-side rendering webpage here.",
-            "Use: Java, Javascript, Html, CSS, Angular JS"
+            "At Arrive, I joined Design and implement logic circuit target in telecommunication application."
           ],
-          logo: vng
+          logo: arrive
         },
-        {
-          title: "Smartdatics",
-          subtitle: "Front-end Development Internship",
-          daterange: "Jun 2017 - Aug 2017",
-          description: [
-            "At Smartdatics, I worked in many projects about studying abroad, bitcoin, ecommerce, etc. because Smardatics is an outsourcing company. My internship happened here and I have learnt about React basics also here.",
-            "Use: Typescript, React, Redux, Html, CSS"
-          ],
-          logo: smartdatics
-        }
       ]
     },
     {
@@ -144,76 +129,11 @@ const App = () => {
             "We decided to use two models of ANN and CNN for deep learning in this field and when we combine two models, the accuracy is quite appreciable at 83 percent."
           ]
         },
-        {
-          title: "BKOOL",
-          subtitle: "Assignment",
-          daterange: "Oct 2016 - Dec 2016",
-          description: [
-            "BKOOL is a programming language that we were assigned to write it a compiler. BKOOL is a very simple language having some basic operations.",
-            "This is a personal assignment of a subject named Principles of Programming Language. I have learned about how programming language works, what exactly is below the things like C or Pascal.",
-            "Use: Scala"
-          ]
-        }
       ]
     },
     {
       title: "ACTIVITIES",
       list: [
-        {
-          title: "CSE Job Fair 2017",
-          subtitle: "Volunteers",
-          daterange: "Nov 2017",
-          description: [
-            "My job here is helping the companies prepare booth and brochures as well as guiding visitors."
-          ]
-        },
-        {
-          title: "Tiếp sức mùa thi 2016",
-          subtitle: "Volunteers",
-          daterange: "Jun 2016",
-          description: [
-            "Volunteer like me instructed examinees registering profiles and designed media publications."
-          ]
-        },
-        {
-          title: "CSE Job Fair 2015",
-          subtitle: "Volunteers",
-          daterange: "Dec 2015",
-          description: [
-            "My job here is helping the companies prepare booth and brochures as well as guiding visitors."
-          ]
-        },
-        {
-          title: "Tư vấn tuyển sinh Về trường 2015",
-          subtitle: "Volunteers",
-          daterange: "Jan 2015",
-          description: [
-            "I came back to high school to introduce students there about my university."
-          ]
-        },
-        {
-          title: "International Friendship Day 2014",
-          subtitle: "Volunteers",
-          daterange: "Dec 2014",
-          description: [
-            "I supported Vietnamese abroad alumni in organizing the festival and guided visitors."
-          ]
-        }
-      ]
-    },
-    {
-      title: "HONORS & AWARDS",
-      list: [
-        {
-          title: "Vingroup Science and Technology Scholarship Program for Overseas Study",
-          daterange: "2021 - 2022",
-          link: "https://scholarships.vinuni.edu.vn/"
-        },
-        {
-          title: "Microsoft Youthspark Scholarship",
-          daterange: "2016 - 2018",
-          link: "https://en.hocbongnusinh.com/microsoft-youthspark-scholarship-for-women-in-technologies-2018-2019.html"
-        }
       ]
     },
     {
@@ -229,11 +149,6 @@ const App = () => {
           subtitle: "issued by ETS",
           daterange: "Jan 2016"
         },
-        {
-          title: "Agile Software Development with Scrum",
-          subtitle: "issued by Axon Active Vietnam",
-          daterange: "Nov 2017"
-        }
       ]
     },
     {
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,22 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Header.scss'
 import {Avatar} from 'antd'
-import avatar from './img/avatar.JPG'
+import avatar from './img/avatar.jpg'
 
-const Header = ({data}) => {
-  const {name, career} = data
+const Header = ({ data }) => {
+  const { name, career } = data;
 
   return (
-    <div className='header'>
-      <div className='cover'>
-        <div className='cover-overlay'/>
-        <Avatar className='avatar' size={200} src={avatar} shape='square'/>
-        <div className='name'><b>{name.first}</b> {name.last}</div>
-        <div className='career'>{career}</div>
+    <div className="header">
+      <div className="cover">
+        <div className="cover-overlay" />
+        <Avatar className="avatar" size={200} src={avatar} shape="square" />
+        <div className="name">
+          <b>{name.first}</b> {name.last}
+        </div>
+        <div className="career">{career}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Header.propTypes = {
   data: PropTypes.object.isRequired
